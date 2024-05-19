6396ce1963bca28b4aa998512f0efa8055ee8d46
geonik72 <161272011+geonik72@users.noreply.github.com>
2024-05-19 22:22:19 +0300

    fixed mobile menu on all views

--- public/scripts/appl.js
+++ public/scripts/appl.js
@@ -1,9 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        const menu = document.querySelector('#mobile-menu');
+        const menuLinks = document.querySelector('.navbar_menu');
 
-const menu = document.querySelector('#mobile-menu')
-const menuLinks = document.querySelector('.navbar_menu')
-
-menu.addEventListener('click', function(){
-    menu.classList.toggle('is-active');
-    menuLinks.classList.toggle('active');
+        if (menu && menuLinks) {
+            menu.addEventListener('click', function(){
+                menu.classList.toggle('is-active');
+                menuLinks.classList.toggle('active');
+            });
+        } 
+    }, 100);
 });
-

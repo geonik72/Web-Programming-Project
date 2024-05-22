d367cd70ef7359183ba9d97121e9f1111b82ef8d
Christos8 <chris02hitas@gmail.com>
2024-05-22 14:18:02 +0300

    random ticket id

--- public/scripts/generate_qr.js
+++ public/scripts/generate_qr.js
@@ -9,6 +9,13 @@ let bookid = document.getElementById("book-id");
 
 
 function generateQR(){
-    qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + "Name: " + bookname.value 
+    qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + "TICKET ID:" + getRandomInt(1000, 9999999) +   "Name: " + bookname.value 
     + "Surname: " + booksurname.value + "Email: " + bookemail.value + "Phone number: " + bookphone.value + "ID: " + bookid.value
 }
+
+
+function getRandomInt(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+  }
